1c297bb237fd7be02d6846900c24f90ce0b2c32c
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-02-25 06:36:47 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   #your code here
+  def initialize(initialize_hash)
+    initialize_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,:weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 end
